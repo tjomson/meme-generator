338c6cabb6ed04f772787d83b6dbdc1814f68a37
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-04-13 13:51:17 +0200

    Hide meme until loaded

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var loadFile = function(event) {
 	var image = document.getElementById('meme');
 	image.src = URL.createObjectURL(event.target.files[0]);
+	document.getElementById("meme").hidden = false;
+	document.getElementById("memeText").hidden = false;
 };
 
 
