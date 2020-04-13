065ba7a666d9203b3cbb489ef5fc63854e5ce2de
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-04-13 12:57:11 +0200

    Write text to meme

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ var loadFile = function(event) {
 	var image = document.getElementById('meme');
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+
+function setMemeText() { 
+	var memeLabel = document.getElementById("memeText");
+	var givenText = document.getElementById("textInput").value + "";
+
+	memeLabel.innerHTML = givenText;
+
+} 
