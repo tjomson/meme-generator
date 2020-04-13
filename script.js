1e7af2137e8e6e35019357f66cf090a65698d081
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-04-13 16:23:16 +0200

    Add comments and back-button

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-var loadFile = function(event) {
+//Metode til at loade det valgte billede
+function loadFile(event) {
 	var image = document.getElementById('meme');
 	image.src = URL.createObjectURL(event.target.files[0]);
+	//Billede og label vises
 	document.getElementById("meme").hidden = false;
 	document.getElementById("memeText").hidden = false;
 };
 
-
+//Metode til at sætte teksten på memet
 function setMemeText() { 
 	var memeLabel = document.getElementById("memeText");
-	var givenText = document.getElementById("textInput").value + "";
+	var givenText = document.getElementById("textInput").value;
 	memeLabel.innerHTML = givenText;
 } 
 
+//Metode til at sætte farven
 function setTextColor() {
 	var memeLabel = document.getElementById("memeText");
 	var colorPicker = document.getElementById("colors");
@@ -19,6 +22,7 @@ function setTextColor() {
 	memeLabel.style.color = color;
 }
 
+//Metode til at sætte skriftstørrelsen
 function setFontSize() {
 	var memeLabel = document.getElementById("memeText");
 	var fontSizeInput = document.getElementById("fontSize");
@@ -26,10 +30,12 @@ function setFontSize() {
 	memeLabel.style.fontSize = fontSize;
 }
 
+//Metode til at sætte placeringen af teksten
 function setPlacement() {
 	var memeLabel = document.getElementById("memeText");
 	var x = document.getElementById("xPlacement").value;
 	var y = document.getElementById("yPlacement").value;
+	//Placeringen er relativ i forhold til billedet, så der benyttes procent
 	memeLabel.style.left = x + "%";
 	memeLabel.style.top = y + "%";
 }
