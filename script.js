7f38aa2977ef3966390da17eeb083d16445064cc
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-04-13 13:23:23 +0200

    Add color picker

--- script.js
+++ script.js
@@ -8,5 +8,11 @@ function setMemeText() {
 	var memeLabel = document.getElementById("memeText");
 	var givenText = document.getElementById("textInput").value + "";
 	memeLabel.innerHTML = givenText;
-
 } 
+
+function setTextColor() {
+	var memeLabel = document.getElementById("memeText");
+	var colorPicker = document.getElementById("colors");
+	var color = colorPicker.value + "";
+	memeLabel.style.color = color;
+}
